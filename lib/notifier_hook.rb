776272978bd3c8a762f5f4a405f05e33c3d4950b
xdite <xuite.joke@gmail.com>
2010-08-15 01:01:36 -0700

    update for truncat issue

--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -1,10 +1,14 @@
+require 'actionpack'
 class NotifierHook < Redmine::Hook::Listener
+
 
   def controller_issues_new_after_save(context = { })
     @project = context[:project]
     @issue = context[:issue]
     @user = @issue.author
-    speak "#{@user.lastname} #{@user.firstname}  created issue 「#{@issue.subject}」. Comment: 「#{truncate_words(@issue.description)}」 http://#{Setting.host_name}/issues/#{@issue.id}"
+    speak "#{@user.lastname} #{@user.firstname} 建立主旨: 「#{@issue.subject}」. 意見: 「#{truncate_words(@issue.de
+scription)}」"
+    speak "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
   end
   
   def controller_issues_edit_after_save(context = { })
@@ -12,34 +16,41 @@ class NotifierHook < Redmine::Hook::Listener
     @issue = context[:issue]
     @journal = context[:journal]
     @user = @journal.user
-    speak "#{@user.lastname} #{@user.firstname}  edited issue 「#{@issue.subject}」. Comment: 「#{truncate_words(@journal.notes)}」.  http://#{Setting.host_name}/issues/#{@issue.id}"
+    speak "#{@user.lastname} #{@user.firstname}  編輯主旨: 「#{@issue.subject}」. 狀態: 「#{@issue.status.name}」.
+意見: 「#{truncate_words(@journal.notes)}」.  "
+    speak " 網址: http://#{Setting.host_name}/issues/#{@issue.id}"
   end
 
   def controller_messages_new_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
     @user = @message.author
-    speak "#{@user.lastname} #{@user.firstname}  wrote a new message 「#{@message.subject}」 on #{@project.name}: 「#{truncate_words(@message.content)}」. http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    speak "#{@user.lastname} #{@user.firstname}  wrote a new message 「#{@message.subject}」 on #{@project.name}: 
+「#{truncate_words(@message.content)}」. http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.
+root.id}#message-#{@message.id}"
   end
   
   def controller_messages_reply_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
     @user = @message.author
-    speak "#{@user.lastname} #{@user.firstname}  replied a message 「#{@message.subject}」 on #{@project.name}: 「#{truncate_words(@message.content)}」. http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    speak "#{@user.lastname} #{@user.firstname}  replied a message 「#{@message.subject}」 on #{@project.name}: 「#
+{truncate_words(@message.content)}」. http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.roo
+t.id}#message-#{@message.id}"
   end
   
   def controller_wiki_edit_after_save(context = { })
     @project = context[:project]
     @page = context[:page]
     @user = @page.content.author
-    speak "#{@user.lastname} #{@user.firstname}  edited the wiki 「#{@page.pretty_title}」 on #{@project.name}. http://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
+    speak "#{@user.lastname} #{@user.firstname}  edited the wiki 「#{@page.pretty_title}」 on #{@project.name}. htt
+p://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
   end
 
 private
 
   def speak(message)
-    system("echo -n #{message} | nc localhost 5678")
+    system("echo -n '#{message}' | nc localhost 5678")
 
   end
 
